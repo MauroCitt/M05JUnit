510dea887ae842e4694e25a01449a303a8c13df6
Esga948 <esga948@vidalibarraquer.net>
2023-05-04 13:50:18 +0200

    Commit Estel Garcés

--- src/test/java/com/mycompany/m05junit/Model/NumeroMasPequeñoTest.java
+++ src/test/java/com/mycompany/m05junit/Model/NumeroMasPequeñoTest.java
@@ -1,4 +1,8 @@
+package com.mycompany.m05junit.Model;
+
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
 import static org.junit.Assert.*;
 
 public class NumeroMasPequeñoTest {
